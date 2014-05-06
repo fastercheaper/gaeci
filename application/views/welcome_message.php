d4f4c7253943a220452f9531bf491a39631b1751
fastercheaper <ravi@boxc.com>
2014-05-06 15:42:29 -0500

    minor change to test push to deploy

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -70,6 +70,8 @@
 	<h1>Welcome to CodeIgniter!</h1>
 
 	<div id="body">
+	    <p>Testing push to deploy</p>
+	    
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
